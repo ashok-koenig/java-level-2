497187ba56103c2056696092928aed9da41aa02d
Ashok S <ashok.s@koenig-solutions.com>
2025-09-03 13:27:28 +0530

    inheritance demo

---
+++ Day1/OOP_Concepts/src/InheritanceDemo.java
@@ -0,0 +1,34 @@
+class Person{
+    String name;
+    int age;
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    void display(){
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
+    }
+}
+
+class Manager extends Person{
+    String dept;
+    Manager(String name, int age, String dept){
+        super(name, age);
+        this.dept = dept;
+    }
+    // Overriding
+    void display(){
+        super.display();
+//        System.out.println("Name: "+ name);
+//        System.out.println("Age: "+ age);
+        System.out.println("Department: "+ dept);
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Manager obj = new Manager("Ram", 25, "IT");
+        obj.display();
+    }
+}
