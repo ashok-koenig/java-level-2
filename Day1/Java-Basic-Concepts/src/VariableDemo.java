545f4a762035100a660f261d425dc2468d26513e
Ashok S <ashok.s@koenig-solutions.com>
2025-09-03 11:28:27 +0530

    primitive variables

---
+++ Day1/Java-Basic-Concepts/src/VariableDemo.java
@@ -0,0 +1,22 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        // Primitive data type
+        int age = 25;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isEmployed = true;
+        byte smallNumber = 100;
+        short shortNumber = 10000;
+        long largeNumber = 97986677777L;
+        float floatNumber = 10.90f;
+
+        System.out.println("Age: " + age );
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: "+ grade);
+        System.out.println("Employed: "+ isEmployed);
+        System.out.println("Small Number: "+ smallNumber);
+        System.out.println("Short Number: "+ shortNumber);
+        System.out.println("Large Number: "+ largeNumber);
+        System.out.println("Float Number: "+ floatNumber);
+    }
+}
